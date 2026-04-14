6305a341c2da7c6850feb46ea050283fbb7aefd9
Ceale <clxh100100@hotmail.com>
2026-04-14 09:32:42 +0800

    更新依赖

---
+++ script/build.ts
@@ -0,0 +1,35 @@
+import { build, BuildConfig } from "bun"
+
+// const workerString = await (
+//         await build({
+//             entrypoints: [ "worker-sub.ts" ],
+//             target: "node",
+//             format: "esm",
+//             packages: "external"
+//         })
+//     )
+//     .outputs[0]
+//     .text()
+
+const config: BuildConfig = {
+    entrypoints: [ "index.ts", "worker-sub.ts" ],
+    target: "node",
+    packages: "external",
+    define: {
+        // "globalThis.WORKER_PATH": JSON.stringify("data:text/javascript," + encodeURIComponent(workerString))
+        // "globalThis.WORKER_PATH": JSON.stringify("data:text/javascript;base64," + Buffer.from(workerString).toBase64())
+        // "globalThis.WORKER_PATH": JSON.stringify(workerString)
+    }
+}
+
+await build({
+    ...config,
+    format: "esm",
+    outdir: "lib/esm/",
+})
+
+await build({
+    ...config,
+    format: "cjs",
+    outdir: "lib/cjs/",
+})
